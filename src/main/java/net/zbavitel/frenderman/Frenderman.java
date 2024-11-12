5734a4a4cdfab5497b77e485b05c76cd8f56b95b
Cris Zbavitel <criszbav@gmail.com>
2024-11-12 17:58:21 -0500

    Initial commit

---
+++ src/main/java/net/zbavitel/frenderman/Frenderman.java
@@ -0,0 +1,16 @@
+package net.zbavitel.frenderman;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Frenderman implements ModInitializer {
+	public static final String MOD_ID = "frenderman";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/zbavitel/frenderman/FrendermanClient.java
@@ -0,0 +1,6 @@
+package net.zbavitel.frenderman;
+import net.fabricmc.api.ClientModInitializer;
+public class FrendermanClient  implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {}
+}
