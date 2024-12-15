1707d1a4cd56c6812e9d42b353274634545cccab
Cris Zbavitel <criszbav@gmail.com>
2024-12-15 17:59:11 -0500

    Init repo

---
+++ src/main/java/net/zbavitel/frenderman/frendermanClient.java
@@ -0,0 +1,10 @@
+package net.zbavitel.frenderman;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class frendermanClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
